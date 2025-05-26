87afe3d1d8ec41c37abd5a88a9b6438b15b8fa68
Luísa <lumendes2001@gmail.com>
2025-05-26 13:26:23 -0300

    removing unused imports

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
 import type { ReactNode } from "react";
 
 interface CartItem {
